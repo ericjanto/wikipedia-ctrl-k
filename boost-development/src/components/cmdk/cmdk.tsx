762d0f34143ebdc1b14e7def81ec86231bb8577d
Eric Janto <e.janto@gmx.de>
2022-12-08 14:32:41 +0000

    Enable gpt communication

--- boost-development/src/components/cmdk/cmdk.tsx
+++ boost-development/src/components/cmdk/cmdk.tsx
@@ -1,9 +1,32 @@
+/*global chrome*/
 import React from 'react'
 import * as Popover from '@radix-ui/react-popover'
 import { Command } from 'cmdk'
-import { Logo, LinearIcon, FigmaIcon, SlackIcon, YouTubeIcon, RaycastIcon, GitHubIcon } from '../../components'
-import { EyeClosedIcon, FileTextIcon, Half2Icon, MagnifyingGlassIcon, ShuffleIcon } from '@radix-ui/react-icons'
+import { Logo } from '../../components'
+import { ChatBubbleIcon, EyeClosedIcon, FileTextIcon, Half2Icon, MagnifyingGlassIcon, ShuffleIcon } from '@radix-ui/react-icons'
 import { wikidarkmodecss } from '../../styles/wiki-darkmode'
+
+window.addEventListener("message", (e) => {
+  const payload = e.data
+  if (payload.includes("ANSWER=")) {
+    const answer = e.data.replace("ANSWER=", "")
+    console.log("Received answer:", answer)
+  } else if (payload.includes("ERROR=")) {
+    const answer = e.data.replace("ERROR=", "")
+    console.log("Encountered error:", answer)
+  }
+})
+
+function askGeneralQuestion(query: string) {
+  console.log(query)
+  // Post query to window
+  window.postMessage("QUERY=" + query)
+  // Move to react with useState for answer
+
+  // Display answer in frontend
+
+  // Handle login error
+}
 
 function searchWiki(query: string) {
   window.location.assign('https://en.wikipedia.org/wiki/' + query.replace(' ', '_'))
@@ -81,6 +104,7 @@ function QueryField({ inputRef, queryHandlerId, setVisibility }: {
 }) {
   const [query, setQuery] = React.useState('')
 
+
   const handleSubmission = function (e: any) {
     if (!e) e = window.event;
     var keyCode = e.code || e.key;
@@ -88,7 +112,9 @@ function QueryField({ inputRef, queryHandlerId, setVisibility }: {
       if (query) {
         switch (queryHandlerId) {
           case "searchWiki": searchWiki(query)
-            break;
+            break
+          case "askGeneralQuestion": askGeneralQuestion(query)
+            break
           default: console.log("Invalid queryHandlerId: ", queryHandlerId)
         }
         setVisibility(false)
@@ -137,11 +163,6 @@ export function RaycastCMDK() {
     inputRef?.current?.focus()
   }, [])
 
-  function handleWikiSearch(query: string) {
-    console.log(query)
-    window.location.assign('https://en.wikipedia.org/wiki/' + query.replace(' ', '_'))
-  }
-
   function activateQueryField(queryHandlerId: string) {
     setShowQueryInput(true)
     queryInputRef?.current?.focus()
@@ -149,7 +170,6 @@ export function RaycastCMDK() {
   }
 
   const [queryHandlerId, setQueryHandlerId] = React.useState('Please change the query handler id where necesseray')
-  const [focusActive, setFocusActive] = React.useState(false)
 
   return (
     <div id='cmdk' className="raycast">
@@ -232,20 +252,17 @@ export function RaycastCMDK() {
               Toggle Dark Mode
             </Item>
           </Command.Group>
-          <Command.Group heading="Other">
-            <Item isCommand value="Clipboard History">
+          <Command.Group heading="AI Helper">
+            <Item value="Ask General Question" onSelect={activateQueryField} queryHandlerId="askGeneralQuestion">
               <Logo>
-                <ClipboardIcon />
+                <ChatBubbleIcon
+                  style={{
+                    width: 12,
+                    height: 12,
+                  }}
+                />
               </Logo>
-              Clipboard History
-            </Item>
-            <Item isCommand value="Import Extension">
-              <HammerIcon />
-              Import Extension
-            </Item>
-            <Item isCommand value="Manage Extensions">
-              <HammerIcon />
-              Manage Extensions
+              Ask General Question
             </Item>
           </Command.Group>
         </Command.List>
@@ -284,7 +301,7 @@ function Item({
   return (
     <Command.Item value={value} onSelect={() => { onSelect ? onSelect(queryHandlerId ? queryHandlerId : 'qhid not defined') : console.log('Please define handler for this item.') }}>
       {children}
-      <span cmdk-raycast-meta="">{isCommand ? 'Command' : 'Application'}</span>
+      <span cmdk-raycast-meta="">{isCommand ? 'Command' : 'Information Retrieval'}</span>
     </Command.Item>
   )
 }
@@ -386,24 +403,6 @@ function SubItem({ children, shortcut }: { children: React.ReactNode; shortcut: 
   )
 }
 
-function TerminalIcon() {
-  return (
-    <svg
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    >
-      <polyline points="4 17 10 11 4 5"></polyline>
-      <line x1="12" y1="19" x2="20" y2="19"></line>
-    </svg>
-  )
-}
-
 function RaycastLightIcon() {
   return (
     <svg width="1024" height="1024" viewBox="0 0 1024 1024" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -411,19 +410,6 @@ function RaycastLightIcon() {
         fillRule="evenodd"
         clipRule="evenodd"
         d="M934.302 511.971L890.259 556.017L723.156 388.902V300.754L934.302 511.971ZM511.897 89.5373L467.854 133.583L634.957 300.698H723.099L511.897 89.5373ZM417.334 184.275L373.235 228.377L445.776 300.923H533.918L417.334 184.275ZM723.099 490.061V578.209L795.641 650.755L839.74 606.652L723.099 490.061ZM697.868 653.965L723.099 628.732H395.313V300.754L370.081 325.987L322.772 278.675L278.56 322.833L325.869 370.146L300.638 395.379V446.071L228.097 373.525L183.997 417.627L300.638 534.275V634.871L133.59 467.925L89.4912 512.027L511.897 934.461L555.996 890.359L388.892 723.244H489.875L606.516 839.892L650.615 795.79L578.074 723.244H628.762L653.994 698.011L701.303 745.323L745.402 701.221L697.868 653.965Z"
-        fill="#FF6363"
-      />
-    </svg>
-  )
-}
-
-function RaycastDarkIcon() {
-  return (
-    <svg width="1024" height="1024" viewBox="0 0 1024 1024" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <path
-        fillRule="evenodd"
-        clipRule="evenodd"
-        d="M301.144 634.799V722.856L90 511.712L134.244 467.804L301.144 634.799ZM389.201 722.856H301.144L512.288 934L556.34 889.996L389.201 722.856ZM889.996 555.956L934 511.904L512.096 90L468.092 134.052L634.799 300.952H534.026L417.657 184.679L373.605 228.683L446.065 301.144H395.631V628.561H723.048V577.934L795.509 650.395L839.561 606.391L723.048 489.878V389.105L889.996 555.956ZM323.17 278.926L279.166 322.978L326.385 370.198L370.39 326.145L323.17 278.926ZM697.855 653.61L653.994 697.615L701.214 744.834L745.218 700.782L697.855 653.61ZM228.731 373.413L184.679 417.465L301.144 533.93V445.826L228.731 373.413ZM578.174 722.856H490.07L606.535 839.321L650.587 795.269L578.174 722.856Z"
         fill="#FF6363"
       />
     </svg>
@@ -469,37 +455,5 @@ function StarIcon() {
         strokeLinejoin="round"
       />
     </svg>
-  )
-}
-
-function ClipboardIcon() {
-  return (
-    <div cmdk-raycast-clipboard-icon="">
-      <svg width="32" height="32" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path
-          d="M6.07512 2.75H4.75C3.64543 2.75 2.75 3.64543 2.75 4.75V12.25C2.75 13.3546 3.64543 14.25 4.75 14.25H11.25C12.3546 14.25 13.25 13.3546 13.25 12.25V4.75C13.25 3.64543 12.3546 2.75 11.25 2.75H9.92488M9.88579 3.02472L9.5934 4.04809C9.39014 4.75952 8.73989 5.25 8 5.25V5.25C7.26011 5.25 6.60986 4.75952 6.4066 4.04809L6.11421 3.02472C5.93169 2.38591 6.41135 1.75 7.07573 1.75H8.92427C9.58865 1.75 10.0683 2.3859 9.88579 3.02472Z"
-          stroke="currentColor"
-          strokeWidth="1.5"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-        />
-      </svg>
-    </div>
-  )
-}
-
-function HammerIcon() {
-  return (
-    <div cmdk-raycast-hammer-icon="">
-      <svg width="32" height="32" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path
-          d="M6.73762 6.19288L2.0488 11.2217C1.6504 11.649 1.6504 12.3418 2.0488 12.769L3.13083 13.9295C3.52923 14.3568 4.17515 14.3568 4.57355 13.9295L9.26238 8.90071M6.73762 6.19288L7.0983 5.80605C7.4967 5.37877 7.4967 4.686 7.0983 4.25872L6.01627 3.09822L6.37694 2.71139C7.57213 1.42954 9.50991 1.42954 10.7051 2.71139L13.9512 6.19288C14.3496 6.62017 14.3496 7.31293 13.9512 7.74021L12.8692 8.90071C12.4708 9.328 11.8248 9.328 11.4265 8.90071L11.0658 8.51388C10.6674 8.0866 10.0215 8.0866 9.62306 8.51388L9.26238 8.90071M6.73762 6.19288L9.26238 8.90071"
-          stroke="currentColor"
-          strokeWidth="1.5"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-        />
-      </svg>
-    </div>
   )
 }
